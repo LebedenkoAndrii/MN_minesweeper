8a3f9774c56e7c98890e0e584d204aa0032cd460
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-04-27 23:14:15 +0300

    no message

---
+++ src/components/Status/Status.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from "react";
+import { Options, GameStatus } from "../lib/minesweeper";
+
+type StatusProps = Options & {
+  status: GameStatus;
+  moveCount: number;
+};
+
+const Status: React.FC<StatusProps> = (props) => {
+  let resolution = <span>In progress</span>;
+  if (props.status === GameStatus.Won) {
+    resolution = <code>Victory!</code>;
+  } else if (props.status === GameStatus.Lost) {
+    resolution = <code>Disaster!</code>;
+  }
+
+  const facts: { [k: string]: any } = {
+    status: resolution,
+    moveCount: props.moveCount,
+    mineCount: props.mineCount,
+    grid: `${props.width} x ${props.height}`,
+  };
+
+  return (
+    <table>
+      {Object.keys(facts).map((key) => (
+        <tr>
+          <td>
+            <code>{key}</code>
+          </td>
+          <td>
+            <code>{facts[key]}</code>
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
+};
+
+export default Status;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./components/lib/store";
+import App from "./App";
+import "font-awesome/css/font-awesome.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
